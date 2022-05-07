69619cb11423a9a5a74fb33501808d6f9340d229
dinoweblog <93374756+dinoweblog@users.noreply.github.com>
2022-05-07 23:32:00 +0530

    toggle button added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,66 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { Stopwatch } from "./Stopwatch";
 import { Timer } from "./Timer";
 
+const Div = styled.div`
+  width: 50%;
+  margin: auto;
+  margin-top: 50px;
+
+  .timer_stopwatch {
+    border: 1px solid blue;
+  }
+  .buttons {
+    display: flex;
+
+    button {
+      width: 50%;
+      padding: 14px;
+      border: none;
+      border-bottom: 3px white solid;
+      background-color: transparent;
+    }
+
+    .active {
+      border-bottom: 3px red solid;
+    }
+  }
+`;
+
 export const Home = () => {
+  const [show, setShow] = useState("timer");
+  const [timerActiveClass, setTimerActiveClass] = useState("");
+  const [stopwatchActiveClass, setStopwatchActiveClass] = useState("");
   return (
-    <div>
+    <Div>
       <h1>Time & Stopwatch</h1>
-      <Timer />
-      <Stopwatch />
-    </div>
+      <div className="timer_stopwatch">
+        <div className="buttons">
+          <button
+            className={timerActiveClass}
+            onClick={() => {
+              setShow("timer");
+              setTimerActiveClass("active");
+              setStopwatchActiveClass("");
+            }}
+          >
+            Timer
+          </button>
+          <button
+            className={stopwatchActiveClass}
+            onClick={() => {
+              setShow("stopwatch");
+              setTimerActiveClass("");
+              setStopwatchActiveClass("active");
+            }}
+          >
+            Stopwatch
+          </button>
+        </div>
+        {show === "timer" ? <Timer /> : <Stopwatch />}
+      </div>
+    </Div>
   );
 };
